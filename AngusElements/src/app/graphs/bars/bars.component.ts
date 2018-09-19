33776d2e9c77e6256645064df9d9309cadb61493
Viacheslav Blinder <jviaches@gmail.com>
2018-09-18 23:16:22 -0500

    Rendered data for bar chart

--- AngusElements/src/app/graphs/bars/bars.component.ts
+++ AngusElements/src/app/graphs/bars/bars.component.ts
@@ -11,6 +11,7 @@ import * as d3Axis from 'd3-axis';
   styleUrls: ['./bars.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
+
 export class BarsComponent implements OnInit {
 
   title = 'Bar Chart';
@@ -24,10 +25,7 @@ export class BarsComponent implements OnInit {
   private svg: any;
   private g: any;
 
-  private dataset: any;
-
-  constructor() { 
-    this.dataset = [12, 19, 8, 17, 22, 9, 15, 12, 22, 25, 17, 12, 25, 16];
+  constructor() {     
   }
 
   ngOnInit() {
@@ -49,8 +47,8 @@ export class BarsComponent implements OnInit {
   private initAxis() {
     this.x = d3Scale.scaleBand().rangeRound([0, this.width]).padding(0.1);
     this.y = d3Scale.scaleLinear().rangeRound([this.height, 0]);
-    this.x.domain(this.dataset.map((d) => d.letter));
-    this.y.domain([0, d3Array.max(this.dataset, (d) => d.frequency)]);
+    this.x.domain(statistics.map((d) => d.letter));
+    this.y.domain([0, d3Array.max(statistics, (d) => d.frequency)]);
   }
 
   private drawAxis() {
@@ -72,7 +70,7 @@ export class BarsComponent implements OnInit {
 
   private drawBars() {
     this.g.selectAll('.bar')
-      .data(this.dataset)
+      .data(statistics)
       .enter().append('rect')
       .attr('class', 'bar')
       .attr('x', (d) => this.x(d.letter))
@@ -81,3 +79,38 @@ export class BarsComponent implements OnInit {
       .attr('height', (d) => this.height - this.y(d.frequency));
   }
 }
+
+
+export interface Frequency {
+  letter: string;
+  frequency: number;
+}
+
+export const statistics: Frequency[] = [
+  {letter: 'A', frequency: .08167},
+  {letter: 'B', frequency: .01492},
+  {letter: 'C', frequency: .02782},
+  {letter: 'D', frequency: .04253},
+  {letter: 'E', frequency: .12702},
+  {letter: 'F', frequency: .02288},
+  {letter: 'G', frequency: .02015},
+  {letter: 'H', frequency: .06094},
+  {letter: 'I', frequency: .06966},
+  {letter: 'J', frequency: .00153},
+  {letter: 'K', frequency: .00772},
+  {letter: 'L', frequency: .04025},
+  {letter: 'M', frequency: .02406},
+  {letter: 'N', frequency: .06749},
+  {letter: 'O', frequency: .07507},
+  {letter: 'P', frequency: .01929},
+  {letter: 'Q', frequency: .00095},
+  {letter: 'R', frequency: .05987},
+  {letter: 'S', frequency: .06327},
+  {letter: 'T', frequency: .09056},
+  {letter: 'U', frequency: .02758},
+  {letter: 'V', frequency: .00978},
+  {letter: 'W', frequency: .02360},
+  {letter: 'X', frequency: .00150},
+  {letter: 'Y', frequency: .01974},
+  {letter: 'Z', frequency: .00074}
+]; 
